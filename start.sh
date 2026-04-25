ebe8265e93ef4240bbaeae51f8031f52f18da24f
ok <ok@example.com>
2026-04-25 20:15:19 +0500

    Move main.py to root for better platform auto-discovery

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # start.sh
-# Run FastAPI with Gunicorn and Uvicorn workers
-gunicorn -w 4 -k uvicorn.workers.UvicornWorker app.main:app --bind 0.0.0.0:$PORT
+export PYTHONPATH=$PYTHONPATH:.
+gunicorn -w 4 -k uvicorn.workers.UvicornWorker main:app --bind 0.0.0.0:$PORT --timeout 120
